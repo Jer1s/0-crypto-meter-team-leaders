7005270f2a166fc62cf4b2563c5c3da56a3e3c13
Jer1s <flqmbl@gmail.com>
2023-05-13 01:34:03 +0900

    Feat: CSS 전역 변수 추가

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -4,6 +4,18 @@ import { Global, css } from '@emotion/react';
 const style = css`
   :root {
     --primary: #00a661;
+    --primary-red: #FD493D;
+    --primary-blue: #4c32ed;
+    --black: #0b0e1b;
+    --gray1: #161c2f;
+    --gray2: #262a38;
+    --gray3: #474b58;
+    --gray4: #616575;
+    --gray5: #848898;
+    --gray6: #A2A7B7;
+    --gray7: #CED2DD;
+    --gray8: #E7E9F0;
+    --gray9: #F5F8F9;
     --background: #f5f8f9;
   }
 
