68fa163df9abae6005a941840a53c4c4fd4d1fc9
임병욱 <bwlimtony@gmail.com>
2023-05-13 17:22:12 +0900

    feat: main container 컴포넌트 생성

---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,42 @@
+import styled from '@emotion/styled';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Contianer = styled.div`
+  max-width: 137.9rem;
+  max-height: 73.3rem;
+  border-radius: 2.5rem;
+  background-color: #fff;
+  padding: 3.6rem 4.8rem 8rem;
+  margin-bottom: 2.4rem;
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  height: 9.9rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: 0.1rem solid black;
+  padding-bottom: 3.2rem;
+`;
+
+const BodyContainer = styled.div`
+  width: 100%;
+`;
+
+const MainContainer = ({ children }) => {
+  const [header, body] = React.Children.toArray(children);
+  return (
+    <Contianer>
+      <HeaderContainer>{header.props.children}</HeaderContainer>
+      <BodyContainer>{body}</BodyContainer>
+    </Contianer>
+  );
+};
+
+MainContainer.PropsTypes = {
+  children: PropTypes.object.isRequired,
+};
+
+export default MainContainer;
