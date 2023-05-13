f56ef01cb637dc46b94cb322a2b0b3ead6590ffa
임병욱 <bwlimtony@gmail.com>
2023-05-13 17:36:03 +0900

    chore: global 색상(#fff) 변수 추가 후 수정

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -4,18 +4,19 @@ import { Global, css } from '@emotion/react';
 const style = css`
   :root {
     --primary: #00a661;
-    --primary-red: #FD493D;
+    --primary-red: #fd493d;
     --primary-blue: #4c32ed;
     --black: #0b0e1b;
+    --white: #ffffff;
     --gray1: #161c2f;
     --gray2: #262a38;
     --gray3: #474b58;
     --gray4: #616575;
     --gray5: #848898;
-    --gray6: #A2A7B7;
-    --gray7: #CED2DD;
-    --gray8: #E7E9F0;
-    --gray9: #F5F8F9;
+    --gray6: #a2a7b7;
+    --gray7: #ced2dd;
+    --gray8: #e7e9f0;
+    --gray9: #f5f8f9;
     --background: #f5f8f9;
   }
 
@@ -30,7 +31,7 @@ const style = css`
   body {
     margin: 0;
     background-color: var(--background);
-    font-family: "Pretendard", sans-serif;
+    font-family: 'Pretendard', sans-serif;
     font-size: 1.6rem;
   }
 
