d1b14a0d28bc6b74c8f49c72f819661dc6860410
Jer1s <flqmbl@gmail.com>
2023-05-15 11:54:52 +0900

    feat: 검색 기록 더미 데이터 추가 및 시나리오 아톰에 currency 필드 추가

--- src/recoils/scenarioData/scenarioDataAtom.jsx
+++ src/recoils/scenarioData/scenarioDataAtom.jsx
@@ -2,7 +2,9 @@ import { atom } from 'recoil';
 
 const scenarioDataAtom = atom({
   key: 'scenarioData',
-  default: { date: { year: 0, month: 0, day: 0 }, price: 0, coinType: 'Bitcoin' },
+  default: {
+    date: { year: 0, month: 0, day: 0 }, currency: 'USD', price: 0, coinType: 'Bitcoin',
+  },
 });
 
 export default scenarioDataAtom;
--- src/recoils/searchHistory/searchHistoryAtom.jsx
+++ src/recoils/searchHistory/searchHistoryAtom.jsx
@@ -2,7 +2,9 @@ import { atom } from 'recoil';
 
 const searchHistoryAtom = atom({
   key: 'searchHistory',
-  default: [],
+  default: [{
+    date: { year: 2022, month: 10, day: 12 }, currency: 'USD', price: 10, coinType: 'Tether',
+  }],
 });
 
 export default searchHistoryAtom;
