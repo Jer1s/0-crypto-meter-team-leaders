115e86b067c664fe5ad973984795a7caac177420
Jer1s <flqmbl@gmail.com>
2023-05-15 11:03:21 +0900

    feat: 팝업창 absolute position으로 변경

--- src/components/Gnb/LocaleCurrencyList.jsx
+++ src/components/Gnb/LocaleCurrencyList.jsx
@@ -6,23 +6,38 @@ import { LOCALE_CURRENCY } from 'utils/constants';
 import { navButtonStyle } from './navButtonStyle';
 
 const popUpSelectorStyle = css`
+  position: absolute;
+  top: 7.2rem;
+  width: 9.06rem;
   cursor: auto;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   gap: 0.4rem;
-  margin-top: 0.4rem;
   padding: 0.4rem;
   background-color: var(--gray9);
 
   @media (max-width: 767px) {
     padding: 0.4rem;
+    top: 6rem; 
+    width: 6.1rem;
+  }
+`;
+
+// 팝업 창 width 문제 해결을 못해서 임시 처리
+const usdWidthStyle = css`
+  width: 10.25rem;
+
+  @media (max-width: 767px) {
+    top: 6rem; 
+    width: 5.7rem;
   }
 `;
 
 const currencyDataStyle = css`
   cursor: pointer;
   display: flex;
+  flex-direction: row;
   align-items: flex-start;
   width: 100%;
   border: none;
@@ -57,7 +72,7 @@ const LocaleCurrencyList = () => {
   };
 
   return (
-    <div css={[navButtonStyle, popUpSelectorStyle]}>
+    <div css={[navButtonStyle, popUpSelectorStyle, (localeCurrency === 'USD') && usdWidthStyle]}>
       {Object.keys(LOCALE_CURRENCY).map((key) => {
         const { currencyUnit, currencySign } = LOCALE_CURRENCY[key];
         return (
