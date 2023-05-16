9d579d961d7d4ef4ae38274799444ebbbede4d54
Jer1s <flqmbl@gmail.com>
2023-05-16 17:48:35 +0900

    style

--- src/components/CryptoMarketCapList/index.jsx
+++ src/components/CryptoMarketCapList/index.jsx
@@ -6,17 +6,17 @@ const mainContainerStyle = css`
   padding-bottom: 11.4rem;
 `;
 
-const headerStyle = css`
+const headerTextStyle = css`
+  margin: 0;
   font-size: 2.6rem;
-  line-height: 3.1rem;
   letter-spacing: -0.3px;
 `;
 
 const CryptoMarketCapList = () => {
   return (
     <MainContainer css={mainContainerStyle}>
-      <div key="headerContent" css={headerStyle}>
-        <h2>전체 암호화폐 시세</h2>
+      <div key="headerContent">
+        <h2 css={headerTextStyle}>전체 암호화폐 시세</h2>
       </div>
       <div key="bodyContent">
         <div />
