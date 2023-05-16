529a1e70b9a4a7b9895968bc65d27449d31e2bff
Jer1s <flqmbl@gmail.com>
2023-05-16 18:24:22 +0900

    feat: 리네이밍, 스타일링

--- src/components/CryptoMarketCapList/index.jsx
+++ src/components/CryptoMarketCapList/index.jsx
@@ -6,20 +6,24 @@ const mainContainerStyle = css`
   padding-bottom: 11.4rem;
 `;
 
-const headerTextStyle = css`
+const headerStyle = css`
   margin: 0;
   font-size: 2.6rem;
   letter-spacing: -0.3px;
+`;
+
+const bodyStyle = css`
+  margin-top: 1.6rem;
 `;
 
 const CryptoMarketCapList = () => {
   return (
     <MainContainer css={mainContainerStyle}>
       <div key="headerContent">
-        <h2 css={headerTextStyle}>전체 암호화폐 시세</h2>
+        <h2 css={headerStyle}>전체 암호화폐 시세</h2>
       </div>
       <div key="bodyContent">
-        <div />
+        <div css={bodyStyle}>asd</div>
       </div>
     </MainContainer>
   );
