224553075a6ef2b884de923dd9de6ea4a79c27e3
Jer1s <flqmbl@gmail.com>
2023-05-17 15:43:14 +0900

    feat: 검색 기록 item 범위 수정, 더미 데이터 7개 추가

--- src/components/Gnb/SearchHistoryPopup.jsx
+++ src/components/Gnb/SearchHistoryPopup.jsx
@@ -11,6 +11,7 @@ import PropTypes from 'prop-types';
 import { navButtonStyle } from './navButtonStyle';
 
 const popupStyle = css`
+  z-index: 1;
   cursor: auto;
   display: flex;
   flex-direction: column;
@@ -41,7 +42,7 @@ const historyHeaderStyle = css`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 2.3rem 0 1.7rem;
+  margin: 2rem 0 1.7rem;
   width: 46.4rem;
   height: 2.4rem;
 
@@ -65,9 +66,9 @@ const historyHeaderStyle = css`
 `;
 
 const historyItemsStyle = css`
-  width: 47.2rem;
+  width: 52rem;
   @media (max-width: 767px) {
-    width: 30rem;
+    width: 100%;
   }
 `;
 
@@ -77,14 +78,34 @@ const historyItemStyle = css`
   align-content: flex-start;
   flex-wrap: wrap;
   gap: 0.6rem 1.2rem;
-  margin: 2rem 0;
-  height: 3.9rem;
+  padding: 2rem 2.4rem;
+  width: 47.2rem;
+  height: 8.1rem;
 
-  @media (max-width: 767px) {
-    gap: 0.4rem 1.2rem;
-    margin: 1.6rem 0;
-    height: 5.8rem;
+  cursor: pointer;
+  border: 0;
+  text-align: start;
+  width: 100%;
+
+  @media (hover: hover) {
+    &:hover {
+      background-color: var(--gray9);
+    }
   }
+
+  border-bottom: 0.1rem solid var(--gray8);
+
+  &:last-child {
+    border: none;
+  }
+
+@media (max-width: 767px) {
+  gap: 0.4rem 1.2rem;
+  padding: 0 2rem;
+  height: 5.8rem;
+  padding: 1.6rem 2rem;
+  height: 9.1rem;
+}
 `;
 
 const scenarioDataStyle = css`
@@ -112,32 +133,10 @@ const scenarioResultStyle = css`
   }
 `;
 
-const buttonStyle = css`
-  cursor: pointer;
-  border: 0;
-  padding: 0;
-  text-align: start;
-  width: 100%;
-
-  @media (hover: hover) {
-    &:hover {
-      background-color: var(--gray9);
-    }
-  }
-`;
-
 const headerHrStyle = css`
   margin: 0;
   border: 0.1rem solid var(--gray8);
   width: 100%;
-`;
-
-const hrStyle = css`
-  border: 0.1rem solid var(--gray8);
-
-  &:last-child {
-    border: none;
-  }
 `;
 
 const symoblContainer = css`
@@ -190,33 +189,30 @@ const SearchHistoryPopup = ({ setShowPopup }) => {
           const formattedPreviousPrice = formatPrice(inputPrice);
           const formattedResultPrice = formatPrice(outputPrice);
           return (
-            <button type="button" onClick={() => { return recalculateHistory(item); }} key={item.id} css={buttonStyle}>
-              <div css={historyItemStyle}>
-                <div css={symoblContainer}>
-                  <img src={item.image} css={symbolStyle} alt="USDT Symbol" />
+            <button type="button" onClick={() => { return recalculateHistory(item); }} key={item.id} css={historyItemStyle}>
+              <div css={symoblContainer}>
+                <img src={item.image} css={symbolStyle} alt="USDT Symbol" />
+              </div>
+              <div css={scenarioDataStyle}>
+                {`만약 ${year}년 ${month}월 ${day}일에 ${formattedPreviousPrice}으로`}
+              </div>
+              <div css={scenarioResultStyle}>
+                <div>
+                  {`${COIN_NAME[item.coinType]}을 샀다면,`}
                 </div>
-                <div css={scenarioDataStyle}>
-                  {`만약 ${year}년 ${month}월 ${day}일에 ${formattedPreviousPrice}으로`}
-                </div>
-                <div css={scenarioResultStyle}>
-                  <div>
-                    {`${COIN_NAME[item.coinType]}을 샀다면,`}
-                  </div>
-                  <div>
-                    {`${outputYear}년 ${outputMonth}월 ${outputDay}일에는 `}
-                    <span css={
+                <div>
+                  {`${outputYear}년 ${outputMonth}월 ${outputDay}일에는 `}
+                  <span css={
                       (isSkyrocketed)
                         ? incrementStyle
                         : decrementStyle
                       }
-                    >
-                      {formattedResultPrice}
-                    </span>
-                    {' 입니다.'}
-                  </div>
+                  >
+                    {formattedResultPrice}
+                  </span>
+                  {' 입니다.'}
                 </div>
               </div>
-              <hr css={hrStyle} />
             </button>
           );
         })}
