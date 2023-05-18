8817115e8b6911bf7a7d195446009770e85179ff
Jer1s <flqmbl@gmail.com>
2023-05-18 11:00:43 +0900

    feat: 1920px이상의 반응형 디자인 완성

--- src/components/HomePageLayout.jsx
+++ src/components/HomePageLayout.jsx
@@ -2,24 +2,32 @@
 import { css } from '@emotion/react';
 import PropTypes from 'prop-types';
 
+const layoutStyle = css`
+  max-width: 1920px;
+  margin: 0 auto;
+`;
+
 const sidebarStyle = css`
   position: fixed;
   width: 44.5rem;
   left: 3.6rem;
+  
+  @media (min-width: 1920px) {
+    left: calc(3.6rem + (100vw - 1920px) / 2);
+  }
 `;
 
 const mainStyle = css`
   box-sizing: content-box;
   padding: 1.2rem 3.6rem 1.2rem 50.5rem;
-  margin: 0;
-  margin-right: auto;
+  margin: 0 auto;
   width: calc(100vw - 56rem);
   max-width: 136rem;
 `;
 
 const HomePageLayout = ({ children }) => {
   return (
-    <main>
+    <main css={layoutStyle}>
       {children.map((child) => {
         return (
           child.key === 'sideBar' ? (
