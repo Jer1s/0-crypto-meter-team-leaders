bbb77cba5b8375501b5254396aa30db690941f57
Yejin Shin <shinyejin0212@gmail.com>
2023-05-18 10:42:42 +0900

    feat: buyPrice 숫자만 입력 가능한 기능 구현

--- src/components/CoinScenarioForm/BuyPriceInput.jsx
+++ src/components/CoinScenarioForm/BuyPriceInput.jsx
@@ -4,7 +4,6 @@ import { css } from '@emotion/react';
 import { useRecoilValue } from 'recoil';
 import localeCurrencySelector from 'recoils/localeCurrency/localeCurrencySelector';
 import PropTypes from 'prop-types';
-// import useFormattedPrice from 'hooks/useFormattedPrice';
 import { coinScenarioInputStyle } from './coinScenarioInputStyle';
 
 const buyPriceInputStyle = (buyPrice) => {
@@ -17,17 +16,26 @@ const buyPriceInputStyle = (buyPrice) => {
 const BuyPriceInput = ({ buyPrice, setBuyPrice }) => {
   const { currencyUnit, currencySign } = useRecoilValue(localeCurrencySelector);
 
+  const validateInput = (value) => {
+    const regex = /^[0-9]*$/;
+    return regex.test(value);
+  };
+
   const handleInputChange = (event) => {
     const { value } = event.target;
-    setBuyPrice(Number(value.replace(/,/g, '')));
+    const numberTypeValue = Number(value.replace(/,/g, ''));
+    if (validateInput(numberTypeValue)) {
+      setBuyPrice(numberTypeValue);
+    }
   };
+
   const formatValue = (value) => {
     return value.toLocaleString();
   };
 
   return (
     <div css={[coinScenarioInputStyle, buyPriceInputStyle(buyPrice)]}>
-      <input css={buyPriceInputStyle} className="inputBox" value={formatValue(buyPrice)} onChange={handleInputChange} />
+      <input css={buyPriceInputStyle} className="inputBox" value={formatValue(buyPrice)} onChange={handleInputChange} required />
       <p>
         {currencyUnit}
         {' '}
