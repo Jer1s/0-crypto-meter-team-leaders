4d55ed06f2db2a66d15e46a1ea4a6698190cb376
Jer1s <flqmbl@gmail.com>
2023-05-20 20:08:30 +0900

    refactor: production build시 추천되는 devtool lazy load 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import { RecoilRoot } from 'recoil';
@@ -6,14 +7,34 @@ import HomePage from 'pages/HomePage';
 
 const queryClient = new QueryClient();
 
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import('@tanstack/react-query-devtools/production').then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    }),
+  ),
+)
+
 const App = () => {
+  const [showDevtools, setShowDevtools] = React.useState(false)
+
+  React.useEffect(() => {
+    // @ts-ignore
+    window.toggleDevtools = () => setShowDevtools((old) => !old)
+  }, [])
+
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
         <GlobalStyle />
         <HomePage />
       </RecoilRoot>
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ReactQueryDevtools initialIsOpen />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
     </QueryClientProvider>
   );
 };
