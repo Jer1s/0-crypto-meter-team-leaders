34d3acaf53f51c4e585b5dd45b238793f49cc67a
wooki <bwlimtony@gmail.com>
2023-05-24 23:05:54 +0900

    feat: 소수점 두자리 적용

--- src/components/CoinDetails/CoinScenarioResult.jsx
+++ src/components/CoinDetails/CoinScenarioResult.jsx
@@ -30,7 +30,7 @@ const currentPriceStyle = ({ isSkyrocketed }) => {
   return css`
     display: inline-block;
     margin-right: 1rem;
-    color: ${isSkyrocketed === null ? 'var(--gray5)' : isSkyrocketed ? 'var(--chart-green)' : 'var(--primary-red'};
+    color: ${isSkyrocketed === null ? 'var(--gray5)' : isSkyrocketed ? 'var(--primary)' : 'var(--primary-red'};
   `;
 };
 
@@ -41,7 +41,7 @@ const zeroStyle = css`
 const nullStyle = css`
   color: var(--gray5);
   text-decoration: line-through;
-`
+`;
 
 const currentDateStyle = css`
   color: var(--gray4);
--- src/hooks/useFormattedPrice.jsx
+++ src/hooks/useFormattedPrice.jsx
@@ -8,8 +8,15 @@ const useFormattedPrice = (ignoreException) => {
   const { currencyUnit, currencySign } = useRecoilValue(localeCurrencySelector);
 
   return (price) => {
-    const convertedPrice = currencyConverter(price, localeCurrency);
-    const formattedPrice = convertedPrice?.toLocaleString();
+    const convertedPrice = convertCurrency(price, localeCurrency);
+    let formattedPrice = convertedPrice?.toLocaleString();
+    let checkingdecimal = formattedPrice;
+    checkingdecimal = (checkingdecimal * 100) % 100;
+    if (checkingdecimal !== 0 && checkingdecimal <= 9) {
+      formattedPrice = Number(formattedPrice).toFixed(1);
+    } else if (checkingdecimal !== 0 && checkingdecimal <= 99) {
+      formattedPrice = Number(formattedPrice).toFixed(2);
+    }
 
     if (!ignoreException && localeCurrency === 'KRW') {
       return `${formattedPrice}${currencyUnit}`;
